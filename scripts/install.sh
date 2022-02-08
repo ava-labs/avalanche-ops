fef3a837d732cd0c1724a151258eed312222f392
Gyuho Lee <gyuho.lee@avalabs.org>
2022-02-08 15:15:53 +0800

    install.sh: initial commit
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -xue
+
+if ! [[ "$0" =~ scripts/install.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+cargo install --verbose \
+--path . \
+--bin avalanche-ops-nodes-aws \
+--bin avalanched-aws
+
+# e.g., "${HOME}/.cargo/bin" should in local ${PATH}
+avalanche-ops-nodes-aws --help
+avalanched-aws --help
