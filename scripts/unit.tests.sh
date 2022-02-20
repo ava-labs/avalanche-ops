dc84675064d387da4ff637f3d785b01449048f46
Gyuho Lee <gyuho.lee@avalabs.org>
2022-02-20 04:22:51 -0800

    scripts: rename to "unit.tests.sh"
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- scripts/unit.tests.sh
+++ scripts/unit.tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xue
 
-if ! [[ "$0" =~ scripts/tests.unit.sh ]]; then
+if ! [[ "$0" =~ scripts/unit.tests.sh ]]; then
   echo "must be run from repository root"
   exit 255
 fi
