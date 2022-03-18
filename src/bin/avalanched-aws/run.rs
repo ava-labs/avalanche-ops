659917f3a65f8ff945ad242b0a757cfc6075cdb6
Gyuho Lee <gyuho.lee@avalabs.org>
2022-03-18 09:28:20 -0700

    avalanched: fetch_metrics initial wait time to 2-min
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/bin/avalanched-aws/run.rs
+++ src/bin/avalanched-aws/run.rs
@@ -898,8 +898,8 @@ async fn fetch_metrics(
     cw_namespace: Arc<String>,
     metrics_ep: Arc<String>,
 ) {
-    info!("STEP: starting 'fetch_metrics' after 5-minute");
-    thread::sleep(Duration::from_secs(300));
+    info!("STEP: starting 'fetch_metrics' after 2-minute");
+    thread::sleep(Duration::from_secs(120));
 
     let mut prev_metrics: Option<metrics::Metrics> = None;
     loop {
