07f223f1bbd8c0056abf4b36024ae4e21aaf4ce0
Gyuho Lee <gyuho.lee@avalabs.org>
2022-03-27 17:42:17 +0200

    types/keychain.rs: add empty implementation
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/avalanche/types/keychain.rs
+++ src/avalanche/types/keychain.rs
@@ -1,6 +1,7 @@
 use crate::avalanche::types::key;
 
 /// ref. https://pkg.go.dev/github.com/ava-labs/avalanchego/vms/secp256k1fx#Keychain
+/// ref. https://github.com/ava-labs/avalanchego/blob/v1.7.8/wallet/chain/p/builder.go
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct Keychain {
     pub key: key::Key,
