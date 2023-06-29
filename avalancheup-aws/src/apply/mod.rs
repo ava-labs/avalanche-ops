3d524a6605a1679cd6f5916b8e386c8ea2cecf71
Chloe <chloe.nguyen@avalabs.org>
2023-06-29 00:36:11 -0500

    draft

--- avalancheup-aws/src/apply/mod.rs
+++ avalancheup-aws/src/apply/mod.rs
@@ -1437,7 +1437,7 @@ pub async fn execute(log_level: &str, spec_file_path: &str, skip_prompt: bool) -
 
             log::info!("waiting for anchor nodes bootstrap and ready (to be safe)");
             sleep(Duration::from_secs(15)).await;          
-            async fn read_script(ssh_commands::Vec<ec2::SshCommand>,) {
+            async fn read_script(ssh_commands: Vec<ec2::SshCommand>) {
                 for ssh_command in ssh_commands.iter() {
                     match ssh_command.run("tail -10 /var/log/cloud-init-output.log") {
                         Ok(output) => {
@@ -1454,7 +1454,6 @@ pub async fn execute(log_level: &str, spec_file_path: &str, skip_prompt: bool) -
                     }
                 }
             }
-            tokio::spawn(read_script(ssh_commands));
             let timeout_duration = Duration::from_secs(10);
             let result = tokio::time::timeout(timeout_duration, read_script(ssh_commands)).await;
             if result.is_err() {
